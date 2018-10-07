97a2daed25b8f1534baf0b2fa02690bdbc20037b
Raj Sekhar <krj2033@gmail.com>
2018-10-07 15:07:53 +0530

    add markdown parser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Markdown from 'markdown-it';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    {{result}}
+  `,
+  // templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'markdown-test';
+  md = new Markdown();
+  result;
+  ngOnInit() {
+    this.result = this.md.renderInline('# markdown-it rulezz!, **hi** `world`');
+    console.log(this.result);
+  }
 }
